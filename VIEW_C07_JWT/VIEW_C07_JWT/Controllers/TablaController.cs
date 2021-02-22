bf4a4d6e150f02693db4931df4e48cb3be1dae02
Víctor Alfonso Martí <victor19am@gmail.com>
2021-02-22 15:59:44 +0100

    Agregar archivos de proyecto.

---
+++ VIEW_C07_JWT/VIEW_C07_JWT/Controllers/TablaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VIEW_C07_JWT.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Tabla()
+        {
+            return View();
+        }
+    }
+}
---
+++ VIEW_C07_JWT/VIEW_C07_JWT/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VIEW_C07_JWT.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
